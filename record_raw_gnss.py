06ee130922ef81bb935b4d2ef40b134eb23ef3ab
lbenvenuto91 <lorenzo.benvenuto@gter.it>
2019-06-19 13:34:41 +0200

    definita e testata la funzione RecordRaw

--- record_raw_gnss.py
+++ record_raw_gnss.py
@@ -26,33 +26,6 @@ except IOError as e:
         raise "Other kind of error"
     
 
-sys.exit()
-
-
-
-
-
-
-run_str2str = "/home/pi/Lorenzo/RTKLIB/app/str2str/gcc/str2str -in serial://ttyACM0#ubx -out file:///home/pi/Lorenzo/code/record_raw_gnss/output_ubx/%s &" %(out_ubx)
-print(run_str2str)
-
-os.system(run_str2str)
-a = []
-time.sleep(2)
-processi = filter(lambda p: p.name() == "str2str", psutil.process_iter())
-for i in processi:
-    a.append(i.pid)
-    print (i.name, i.pid, a)
-
-
-process_ID = a[-1] #if there are more str2str procces, the PID are appended in this array. The PID of the str2srt proccess launched by this script is the last element of the array.
-print (process_ID)
-
-time.sleep(time_sec)
-os.system("sudo kill %s" %process_ID)
-
-
-
 def RecordRaw(minutes, serial, rtklib_path):
     ''' Function to record raw GNSS data from a ublox receiver.
         The receiver configuration is not set in this script and must be set using the u-center software
@@ -65,10 +38,30 @@ def RecordRaw(minutes, serial, rtklib_path):
 '''
 
     time_sec = minutes*60
+    out_path = "/home/pi/Lorenzo/code/raw_data_from_ublox/output_ubx"
+    now = datetime.datetime.now()
+    out_ubx = "raw_obs_%s-%s-%s_%s:%s.ubx" % (now.day, now.month, now.year, now.hour, now.minute)
+    out_file = "%s/%s"%(out_path, out_ubx)
+    print(out_file)    
+    run_str2str = "%s -in serial://%s#ubx -out file://%s &" %(rtklib_path, serial, out_file)
+    print(run_str2str)
+    os.system(run_str2str)
+    
+    a = []
+    time.sleep(2)
+    process = filter(lambda p: p.name() == "str2str", psutil.process_iter())
+    for i in process:
+        a.append(i.pid)
+        print (i.name, i.pid, a)
 
 
+    process_ID = a[-1] #if there are more str2str procces, the PID are appended in this array. The PID of the str2srt proccess launched by this script is the last element of the array.
+    print (process_ID)
 
-    pass
+    time.sleep(time_sec)
+    os.system("sudo kill %s" %process_ID)
+
+    
 
 def RinexConverter():
 
@@ -84,15 +77,15 @@ def Hatanaka():
 
 def main():
     print("***************** START SCRIPT *****************")
-    time_min = 3  #minutes
+    time_min = 1  #minutes
     
-    print ("Format dd-mm-yyyy =  %s-%s-%s" % (i.day, i.month, i.year))
-    print ("Format hh:mm:ss = %s:%s:%s" % (i.hour, i.minute, i.second))
-    out_ubx= "raw_obs_%s-%s-%s_%s:%s.ubx" % (i.day, i.month, i.year, i.hour, i.minute)
-    print(out_ubx)
+    #print ("Format dd-mm-yyyy =  %s-%s-%s" % (i.day, i.month, i.year))
+    #print ("Format hh:mm:ss = %s:%s:%s" % (i.hour, i.minute, i.second))
+    
     str2str_path="/home/pi/Lorenzo/RTKLIB/app/str2str/gcc/str2str"  #probably relative paths are better (think about changing absolute paths)
     usbSerial = "ttyACM0" #you can find it in /dev/
-    RecordRaw(time_min, str2str_path )
+    RecordRaw(time_min, usbSerial, str2str_path )
+
 
 
 if __name__ == "__main__":
