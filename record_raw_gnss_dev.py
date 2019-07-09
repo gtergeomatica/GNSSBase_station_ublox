e0524c8519e8353c5cd163762c9b4f8c89572e9c
lbenvenuto91 <lorenzo.benvenuto@gter.it>
2019-07-09 17:42:36 +0200

    aggiunto metodo per conversione da ubx a RINEX

--- record_raw_gnss_dev.py
+++ record_raw_gnss_dev.py
@@ -14,13 +14,15 @@ import errno
 class GNSSReceiver:
     '''This class describes the possibile action that a generic GNSS receiver connected to a microprocessor can do'''
     
-    def __init__(self, model="Ublox neo m8t", serial="ttyACM0", raw_format="ubx",repo_absolute_path="/home/pi/Lorenzo/code/raw_data_from_ublox/"):
+    def __init__(self, out_raw="default_filename", model="Ublox neo m8t", serial="ttyACM0", raw_format="ubx",repo_absolute_path="/home/pi/Lorenzo/code/raw_data_from_ublox/"):
         self.model=model
         self.serial=serial
         self.raw_format=raw_format
+        self.out_raw=out_raw
         self.repo_absolute_path=repo_absolute_path #the path where the repository has been cloned
+    
     def __str__(self):
-        return "\tReceiver: %s\n\tSerial: %s\n\tGNSS raw data format: %s\n"%(self.model,self.serial,self.raw_format)
+        return "\tReceiver: %s\n\tSerial: %s\n\tGNSS raw format: %s\n\tGNSS raw data file: ./output_ubx/%s\n"%(self.model,self.serial,self.raw_format,self.out_raw)
 
 
     def RecordRaw(self,time_min):
@@ -33,14 +35,12 @@ class GNSSReceiver:
     '''
         #time_min = 1 #time for raw data recording
         time_sec = time_min*60
-        out_path = "/home/pi/Lorenzo/code/raw_data_from_ublox/output_ubx"
-        now = datetime.datetime.now()
-        out_ubx = "raw_obs_%s-%s-%s_%s:%s.%s" % (now.day, now.month, now.year, now.hour, now.minute, self.raw_format)
-        out_file = "%s/%s"%(out_path, out_ubx)
+        out_file = "./output_ubx/%s.%s"%(self.out_raw,self.raw_format)
         print(out_file)    
         str2str_path="/home/pi/Lorenzo/RTKLIB/app/str2str/gcc/str2str" #path to str2str executable
         run_str2str = "%s -in serial://%s#%s -out file://%s &" %(str2str_path, self.serial, self.raw_format, out_file)
         print(run_str2str)
+        print("\n************* Start data acquisition *************\n")
         os.system(run_str2str)
         
         a = []
@@ -56,21 +56,32 @@ class GNSSReceiver:
 
         time.sleep(time_sec)
         os.system("sudo kill %s" %process_ID)
+        print("\n************* Stop data acquisition *************\n")
+        return out_file
 
     def FunctionTest(parmeter):
             output_test_param="test_%s" %(parmeter)
             return output_test_param
 
-    def RinexConverter():
+    def RinexConverter(self):
 
         '''Function to convert a raw GNSS file from ubx format to RINEX format using CONVBIN module of rtklib
 
     '''
-
-
-        infile = 
-        outfile = 
-        pass
+        infile = "./output_ubx/%s.%s"%(self.out_raw,self.raw_format)
+        outfile_obs = "./output_rinex/%s.obs"%(self.out_raw)
+        outfile_nav = "./output_rinex/%s.nav"%(self.out_raw)
+        convbin_path="/home/pi/Lorenzo/RTKLIB/app/convbin/gcc/convbin"
+    
+        run_convbin_obs = "%s %s -o %s -n %s"%(convbin_path, infile, outfile_obs, outfile_nav)
+        print("\n************* Conversion ubx --> RINEX *************\n")
+        os.system(run_convbin_obs)
+        print("\n************* Done! *************\n")
+            
+    
+    
+    
+    
     def Hatanaka():
         '''Function to compress a RINEX file using the hatanaka compression format
         
@@ -80,10 +91,19 @@ class GNSSReceiver:
 def main():
     print("\n***************** START SCRIPT *****************\n")
     time_min = 1  #minutes
-    Stazione1=GNSSReceiver() #if not specified the typical characteristics of the gnss receiver are those of NARVALO BOX
-    print(Stazione1)
-    #Stazione1.RecordRaw(time_min) #specify the number of minutes for the raw data recording
+       
+    out_path = "/home/pi/Lorenzo/code/raw_data_from_ublox/output_ubx"
+    now = datetime.datetime.now()
+    out_raw = "raw_obs_%s-%s-%s_%s:%s" % (now.day, now.month, now.year, now.hour, now.minute)
     
+    Stazione1=GNSSReceiver(out_raw) #create the isatnce: if not specified the typical characteristics of the gnss receiver are those of NARVALO BOX
+    print(Stazione1)    
+    
+    Stazione1.RecordRaw(time_min) #specify the number of minutes for the raw data recording
+    
+    Stazione1.RinexConverter()
+    prova = os.listdir("/home/pi/Lorenzo/code/raw_data_from_ublox/output_ubx")
+    print(prova)
     '''
     
     path = "./output_ubx/paperino.ubx"
