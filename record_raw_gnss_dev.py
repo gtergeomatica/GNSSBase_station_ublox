9db503a43b9661916291f95fbf3245e720c3fb06
Lorenzo <lorenzo.benvenuto@gter.it>
2020-09-17 14:08:39 +0200

    aggiunto kill di tutti i processi str2str prima di avviare quello nuovo, e bug fix sul nome delle cartelle

--- record_raw_gnss_dev.py
+++ record_raw_gnss_dev.py
@@ -44,6 +44,7 @@ class GNSSReceiver:
         str2str_path="%sapp/str2str/gcc/str2str"%(self.rtklib_path) #path to str2str executable
         run_str2str = "%s -in serial://%s#%s -out file://%s &" %(str2str_path, self.serial, self.raw_format, out_file)
         print(run_str2str)
+        os.system("sudo killall str2str")   #non sicuro che sia la cosa giusta da fare
         print("\n************* Start data acquisition *************\n")
         os.system(run_str2str)
         
@@ -96,6 +97,14 @@ class GNSSReceiver:
             print(e)
             return False
     
+    def removeBinary(self):
+        try:
+            os.system('rm {}/output_ubx/{}.{}'.format(os.path.dirname(os.path.realpath(__file__)),self.out_raw,self.raw_format))
+            return True
+        except Exception as e:
+            print(e)
+            return False
+    
     
     
     
@@ -107,12 +116,12 @@ class GNSSReceiver:
 
 
 
-def ftpPush(ftp,path,infile):
+def ftpPush(ftp,path,infile,folder_name):
     '''
     Function to send a file to an ftp server
     '''
-    folder_name='%04d-%02d-%02d'%(datetime.utcnow().utctimetuple().tm_year,datetime.utcnow().utctimetuple().tm_mon,datetime.utcnow().utctimetuple().tm_mday)
-    chdir(ftp,path)
+    #folder_name='%04d-%02d-%02d'%(datetime.utcnow().utctimetuple().tm_year,datetime.utcnow().utctimetuple().tm_mon,datetime.utcnow().utctimetuple().tm_mday)
+    chdir(ftp,path,folder_name)
     try:
 
         with open('{}/output_rinex/{}'.format(os.path.dirname(os.path.realpath(__file__)),infile), 'rb') as f:  
@@ -125,14 +134,14 @@ def ftpPush(ftp,path,infile):
         
         return False
 
-def chdir(ftp,dir): 
+def chdir(ftp,dir,folder_name): 
     '''
     Funzione per creare in automatico una cartella giornaliera per i rinex orari
     La funzione controlla che la cartella non esista già e nel caso la crea
     La funzione copia in automatico nella cartella creata i file per lo stile
     '''
 
-    folder_name='%04d-%02d-%02d'%(datetime.utcnow().utctimetuple().tm_year,datetime.utcnow().utctimetuple().tm_mon,datetime.utcnow().utctimetuple().tm_mday)
+    #folder_name='%04d-%02d-%02d'%(datetime.utcnow().utctimetuple().tm_year,datetime.utcnow().utctimetuple().tm_mon,datetime.utcnow().utctimetuple().tm_mday)
     if directory_exists(ftp,dir,folder_name) is False: # (or negate, whatever you prefer for readability)
         ftp.mkd(dir+folder_name)
         path_local='{}/DisplayDirectoryContents'.format(os.path.dirname(os.path.realpath(__file__)))
@@ -333,7 +342,7 @@ def main():
     minutes=datetime.utcnow().utctimetuple().tm_min
     start_time='%04d%03d%02d%02d'%(year,day_of_year,hour,minutes)
     out_raw = "raw_obs_%s_%s" % (author,start_time)
-    nome=rinex302filename('LIGE',start_time,60,1,'MO',False,False)
+    nome_file=rinex302filename('LIGE',start_time,60,1,'MO',False,False)
     
 
     Stazione1=GNSSReceiver(out_raw,model='UBLOX ZED F9P',antenna="HEMISPHERE A45",rtklib_path='/home/pi/RTKLIB_demo5/')#create the isatnce: if not specified the typical characteristics of the gnss receiver are those of NARVALO BOX
@@ -347,10 +356,11 @@ def main():
     
     ftp = FTP(ftp_url)  
     ftp.login(ftp_user, ftp_password) 
-
-    if ftpPush(ftp,remote_folder,nome)== True: #carico il file rinex registrato sul server (il caricamento avviene nel if statement)
-        print('cancello il file sul raspi')
+    folder_name_day='%04d-%02d-%02d'%(datetime.utcnow().utctimetuple().tm_year,datetime.utcnow().utctimetuple().tm_mon,datetime.utcnow().utctimetuple().tm_mday)
+    if ftpPush(ftp,remote_folder,nome_file,folder_name_day)== True: #carico il file rinex registrato sul server (il caricamento avviene nel if statement)
+        print('cancello i file sul raspi')
         Stazione1.removeRinex(nome)
+        Stazione1.removeBinary()
     else:
         print('qualcosa non va...')
     ftp.quit()
